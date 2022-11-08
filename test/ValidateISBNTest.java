9a86d85d71994bf52f4e83cbb3a681d96b5ff102
kpilszak <klaudia.pilszak@gmail.com>
2022-11-08 20:02:42 +0100

    Create red test

--- test/ValidateISBNTest.java
+++ test/ValidateISBNTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ValidateISBNTest {
 
     @Test
-    public void test() {
+    public void checkValidISBN() {
+        fail();
     }
 
 }
